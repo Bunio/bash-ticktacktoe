345f962b6a9d2522cd979dc50de4503091fd5a2a
Jakub Duzyk <jakub.duzyk@lumesse.com>
2018-03-23 21:26:27 +0100

    printTable method

--- ticktacktoe.sh
+++ ticktacktoe.sh
@@ -3,7 +3,7 @@ echo "Ticktacktoe --------------------------"
 
 #-------------
 
-TABLE=("0" "0" "0" "0" "0" "0" "0" "0" "0")
+TABLE=("00" "01" "02" "03" "04" "05" "06" "07" "08")
 
 
 
@@ -14,14 +14,17 @@ function hello {
  echo "Hello"
 }
 
-
+function printTable {
+ for i in {0..2}
+ do
+	 echo "${TABLE[0 + 3*$i]} ${TABLE[1 + 3*$i]} ${TABLE[2 + 3*$i]}"
+ done
+}
 #-------------
 
 
-for i in "${TABLE[@]}"
-do
-echo "${TABLE[i]}"
-done
+
+printTable
 
 
 
